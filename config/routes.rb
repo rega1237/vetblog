bf21c9d239dda3e2f6d43c54ff0f53dfe7628493
rega1237 <rega1237@gmail.com>
2024-03-21 20:15:58 -0400

    create post model, controller, routes and views

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   root "home#index"
+  
+  resources :posts
 end
--- db/migrate/20240321185418_create_posts.rb
+++ db/migrate/20240321185418_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
       t.string :title
-      t.text :content
+      t.text :body
 
       t.timestamps
     end

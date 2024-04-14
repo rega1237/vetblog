288e3e20339602349b776edffa6ffcd4bed7dc58
rega1237 <rega1237@gmail.com>
2024-04-13 21:42:56 -0400

    add google button, change flash message and add sign_in and sign_out link

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,10 +3,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       @user = User.from_omniauth(request.env['omniauth.auth'])
 
       if @user.persisted?
-        sign_in_and_redirect @user
+        flash[:notice] = "success"
+        sign_in_and_redirect @user, event: :authentication
       else
         session['devise.google_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
-        redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
+        flash[:notice] = "failure"
+        redirect_to new_user_registration_url
       end
   end
 end
